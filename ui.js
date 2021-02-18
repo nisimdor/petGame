1aeaaebb0dd21fe6aba607636f0b04be899ac49e
nisimdor <nisimdor@gmail.com>
2021-02-18 21:57:50 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,13 +1,13 @@
-export const modFox = (state) =>
+const modFox = (state) =>
   (document.querySelector(".fox").className = `fox fox-${state}`);
 
-export const modScene = (state) =>
+const modScene = (state) =>
   (document.querySelector(".game").className = `game ${state}`);
 
-export const tooglePoopBag = (show) =>
+const tooglePoopBag = (show) =>
   document.querySelector(".poop-bag").classList.toggle("hidden", !show);
 
-export const writeModal = (text = "") => {
+const writeModal = (text = "") => {
   document.querySelector(
     ".modal"
   ).innerHTML = `<div class="modal-inner">${text}</div>`;
